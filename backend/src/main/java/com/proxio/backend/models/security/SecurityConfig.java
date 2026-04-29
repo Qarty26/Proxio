e73049997a949723c52f8ea9c56d6844f332583c
Qarty26 <rzvandmir03@gmail.com>
2026-04-29 14:16:04 +0300

    Fix for 401 cookie issues

--- backend/src/main/java/com/proxio/backend/models/security/SecurityConfig.java
+++ backend/src/main/java/com/proxio/backend/models/security/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.proxio.backend.models.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,17 +23,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+
 
 //                TODO: fill permissions once backend is complete.
 //                 Here are just some examples that should be redone correctly
+        http
+                .csrf(csrf -> csrf
 
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+                )
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class) // Adăugăm un filtru custom (vezi pasul 2)
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/api/users/**").permitAll()
-                                .requestMatchers("/api/locations/**").hasRole("ADMIN")
+                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/api/locations/**").hasRole("BANANA")
                         .anyRequest().authenticated()
-                );
+                )
+                .httpBasic(Customizer.withDefaults());
 
             //TODO: These can be uncommented once those enpoints and pages exist.
             // For now they get stuck in a loop since they do not exist
